ade8f6b2b07f6093cb8c91aad741393e0e0e5aad
Himanshu <masanthimanshu@gmail.com>
2026-04-30 09:10:31 +0530

    updates

---
+++ core/s3_client.js
@@ -0,0 +1,19 @@
+import { randomUUID } from "node:crypto";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+
+const s3Client = new S3Client({ region: process.env.CURRENT_AWS_REGION });
+
+export async function generateUploadUrl() {
+  const key = `${randomUUID()}.png`;
+
+  const command = new PutObjectCommand({
+    Key: key,
+    ContentType: "image/png",
+    Bucket: process.env.BUCKET_NAME,
+  });
+
+  const uploadUrl = await getSignedUrl(s3Client, command, { expiresIn: 300 });
+
+  return { uploadUrl, key };
+}
